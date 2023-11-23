c983f479fe1941bc9b15cacd357b8bd5248da446
Amilios\Tmilios <anastasios.milios@ext.ec.europa.eu>
2023-11-23 15:05:38 +0200

    Displaying nodes diff

--- src/js/script.js
+++ src/js/script.js
@@ -1,84 +1,228 @@
-// script.js
-function showLoadingSpinner() {
-    $('#loadingSpinner').show();  
+// App state and configuration
+const appConfig = {
+    tagsBaseUrl: 'https://api.github.com/repos/OP-TED/eForms-SDK',
+    fieldsBaseUrl: 'https://raw.githubusercontent.com/OP-TED/eForms-SDK'
+};
+
+const appState = {
+    sortedData: [],
+    versionData: [],
+    comparisonData: []
+};
+
+// DOM elements cache
+const domElements = {
+    loadingSpinner: $('#loadingSpinner'),
+    apiStatus: $('#apiStatus'),
+    xmlStructureTree: $('#xmlStructureTree'),
+    tagsDropdown: $('#tagsDropdown'),
+    comparisonDropdown: $('#comparisonDropdown')
+};
+
+// Show and hide loading spinner
+function toggleLoadingSpinner(show) {
+    if (show) {
+        domElements.loadingSpinner.show();
+    } else {
+        domElements.loadingSpinner.hide();
+    }
 }
 
-function hideLoadingSpinner() {
-    $('#loadingSpinner').hide();
-}
-
+// Clear API status text
 function clearApiStatus() {
-    $('#apiStatus').text('');
+    domElements.apiStatus.text('');
 }
 
+// Build tree data for jstree from XML structure
 function buildTreeData(xmlStructure) {
-    return xmlStructure.map(function(node) {
-        var treeNode = {
-            id: node.id,
-            parent: node.parentId ? node.parentId : "#",
-            text: node.id,
-            state: {
-                opened: true
-            }
-        };
-        return treeNode;
-    });
+    return xmlStructure.map(node => ({
+        id: node.id,
+        parent: node.parentId || "#",
+        text: node.id,
+        state: {
+            opened: true
+        }
+    }));
 }
 
+// Initialize jstree with XML structure
 function initializeTree(xmlStructure) {
-    $('#xmlStructureTree').jstree({
-        'core': {
-            'data': buildTreeData(xmlStructure)
+    domElements.xmlStructureTree.jstree({
+        core: {
+            data: buildTreeData(xmlStructure)
         }
     });
 }
 
-function populateDropdown() {
-    showLoadingSpinner();
+// Fetch and display fields content
+async function fetchAndDisplayFieldsContent(tagName, isTagsDropdown) {
+    toggleLoadingSpinner(true);
     clearApiStatus();
-    $.get('https://api.github.com/repos/OP-TED/eForms-SDK/tags')
-    .done(function(data) {
-        var dropdown = $('#tagsDropdown');
-        dropdown.empty(); 
+    
+    const url = `${appConfig.fieldsBaseUrl}/${tagName}/fields/fields.json`;
+    
+    try {
+        const response = await $.ajax({ url, dataType: 'json' });
+        const fieldsData = response;
         
-        $.each(data, function(index, item) {
-            dropdown.append($('<option>', {
-                value: item.name,
-                text: item.name
-            }));
+        if (isTagsDropdown) {
+            appState.versionData = fieldsData.xmlStructure;
+            fieldsData.xmlStructure[0].version =fieldsData.sdkVersion;
+
+        } else {
+            fieldsData.xmlStructure[0].version =fieldsData.sdkVersion;
+            appState.comparisonData = fieldsData.xmlStructure;
+            if (appState.versionData && appState.comparisonData) {
+                const treeData = compareXMLStructures(appState.comparisonData, appState.versionData);
+                console.log('treeData',treeData)
+                initializeTree(treeData);
+            }
+        }
+
+        domElements.apiStatus.text(`API call succeeded for file version "${tagName}"`);
+    } catch (error) {
+        domElements.apiStatus.text(`API call failed for file version "${tagName}"`);
+        console.error('Error fetching and displaying fields content:', error);
+    } finally {
+        toggleLoadingSpinner(false);
+    }
+}
+
+
+function buildTreeStructure(xmlStructure) {
+    let tree = {};
+    xmlStructure.forEach(node => {
+        // If node has a parentId, add it to the tree under the parentId
+        if (node.parentId) {
+            if (!tree[node.parentId]) {
+                tree[node.parentId] = [];
+            }
+            tree[node.parentId].push(node);
+        } else {
+            // If node does not have a parentId, it is a root node
+            tree[node.id] = tree[node.id] || [];
+        }
+    });
+    return tree;
+}
+
+function buildHierarchy(xmlStructure) {
+    let hierarchy = {};
+    let root = null;
+
+    // First, find the root node
+    xmlStructure.forEach(node => {
+        if (!node.parentId) {
+            root = node; // Assume there is only one root in the structure
+            hierarchy[node.id] = { ...node, children: [] };
+        }
+    });
+
+    // Then, build the hierarchy
+    xmlStructure.forEach(node => {
+        if (node.parentId) {
+            if (!hierarchy[node.parentId]) {
+                hierarchy[node.parentId] = { children: [] };
+            }
+            let childNode = { ...node, children: [] };
+            hierarchy[node.parentId].children.push(childNode);
+            hierarchy[node.id] = childNode;
+        }
+    });
+
+    return { root, hierarchy };
+}
+
+function compareXMLStructures(oldXmlStructure, newXmlStructure) {
+    let comparisonResults = [];
+
+    // Create maps for quick lookups by id
+    let oldMap = new Map(oldXmlStructure.map(node => [node.id, node]));
+    let newMap = new Map(newXmlStructure.map(node => [node.id, node]));
+
+    // Check for removed and unchanged nodes
+    oldXmlStructure.forEach(oldNode => {
+        if (newMap.has(oldNode.id)) {
+            // Node exists in both structures
+            comparisonResults.push({ ...oldNode, nodeChange: 'unchanged' });
+        } else {
+            // Node only exists in old structure
+            comparisonResults.push({ ...oldNode, nodeChange: 'removed' });
+        }
+    });
+
+    // Check for added nodes
+    newXmlStructure.forEach(newNode => {
+        if (!oldMap.has(newNode.id)) {
+            // Node only exists in new structure
+            comparisonResults.push({ ...newNode, nodeChange: 'added' });
+        }
+        // 'Unchanged' nodes are already handled in the previous loop
+    });
+
+    return comparisonResults;
+}
+
+
+
+function findIndexByVersion(versionName) {
+    return sortedData.findIndex(function(item) {
+        return item.name === versionName;
+    });
+}
+
+
+// Populate dropdowns with tags
+async function populateDropdown() {
+    toggleLoadingSpinner(true);
+    clearApiStatus();
+    
+    try {
+        const response = await $.ajax({
+            url: `${appConfig.tagsBaseUrl}/tags`,
+            dataType: 'json'
+        });
+        const data = response.sort((a, b) => b.name.localeCompare(a.name));
+
+        appState.sortedData = data;
+        domElements.tagsDropdown.empty();
+        domElements.comparisonDropdown.empty();
+
+        data.forEach(item => {
+            const option = $('<option>', { value: item.name, text: item.name });
+            domElements.tagsDropdown.append(option.clone());
+            domElements.comparisonDropdown.append(option);
         });
 
-        fetchAndDisplayFieldsContent(data[0].name); 
-    })
-    .fail(function() {
-        hideLoadingSpinner(); 
-        $('#apiStatus').text('API call failed for file version.');
-    });
+        domElements.tagsDropdown.val(data[0].name);
+        domElements.comparisonDropdown.val(data.length > 1 ? data[1].name : data[0].name);
+        
+        await fetchAndDisplayFieldsContent(data[0].name, true);
+        await fetchAndDisplayFieldsContent(data[1].name, false);
+    } catch (error) {
+        domElements.apiStatus.text('API call failed to fetch tags.');
+        console.error('Error populating dropdowns:', error);
+    } finally {
+        toggleLoadingSpinner(false);
+    }
 }
 
-function fetchAndDisplayFieldsContent(tagName) {
-    showLoadingSpinner(); 
-    clearApiStatus(); 
-    $.get(`https://raw.githubusercontent.com/OP-TED/eForms-SDK/${tagName}/fields/fields.json`)
-    .done(function(data) {
-        hideLoadingSpinner(); 
-        var fieldsData = JSON.parse(data); 
-        initializeTree(fieldsData.xmlStructure);
-        $('#apiStatus').text(`API call succeeded for file version "${tagName}"`);
-    })
-    .fail(function() {
-        hideLoadingSpinner();
-        $('#apiStatus').text(`API call failed for file version "${tagName}"`);
-    });
-}
-
-$(document).ready(function() {
+// Document ready function
+$(document).ready(() => {
     populateDropdown();
 });
 
-$(document).on('change', '#tagsDropdown', function() {
-    var selectedTagName = $(this).val();
-    $('#fieldsContent').empty(); 
-    $('#xmlStructureTree').jstree("destroy");
-    fetchAndDisplayFieldsContent(selectedTagName); 
+// Event handlers for dropdown changes
+domElements.tagsDropdown.change(async function() {
+    const selectedTagName = $(this).val();
+    const comparisonTagName = domElements.comparisonDropdown.val();
+    await fetchAndDisplayFieldsContent(selectedTagName, true);
+    await fetchAndDisplayFieldsContent(comparisonTagName, false);
+});
+
+domElements.comparisonDropdown.change(async function() {
+    const selectedTagName = $(this).val();
+    const comparisonTagName = domElements.comparisonDropdown.val();
+    await fetchAndDisplayFieldsContent(selectedTagName, true);
+    await fetchAndDisplayFieldsContent(comparisonTagName, false);
 });
